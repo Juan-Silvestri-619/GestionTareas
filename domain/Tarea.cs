22b7248b28525da3c5e1bc73d817eb7a48df3ad0
Juan-Silvestri-619 <juansilvestri.empleos@gmail.com>
2026-07-13 20:27:32 -0300

    dominio de gestión de tareas

---
+++ domain/Tarea.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Tarea
+    {
+        public int Id { get; private set; }
+        public string Titulo { get; private set; }
+        public string Descripcion { get; private set; }
+        public bool Completada { get; private set; }
+        public DateTime FechaCreacion { get; private set; }
+
+        //Traer información de la base de datos
+        public Tarea(int id, string titulo, string descripcion, bool completada ,DateTime fecha)
+        {
+            Id = id;
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Completada = completada;
+            FechaCreacion = fecha;
+        }
+        //Creamos una nueva tarea
+        public Tarea(string titulo, string descripcion)
+        {
+            Titulo = ValidarTitulo(titulo);
+            Descripcion = descripcion;
+            FechaCreacion = DateTime.Now;
+            Completada = false;
+        }
+
+        public void CambiarTitulo(string nuevoTitulo)
+        {
+            Titulo = ValidarTitulo(nuevoTitulo);
+        }
+        private string ValidarTitulo(string titulo)
+        {
+            if (string.IsNullOrWhiteSpace(titulo))
+                throw new Exception("Error en el título de la actividad");
+
+            return titulo;
+        }
+        public void Completar()
+        {
+            Completada = true;
+        }
+    }
+}
